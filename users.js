d9f5912c70d23f8fd0165576dfa2433c4c59354f
Вячеслав <vyacheslav@MBP-Vaceslav.Dlink>
2018-10-29 21:01:30 +0300

    fix: translated comments

--- users.js
+++ users.js
@@ -2,16 +2,16 @@ var expectedUsers = 1000;
 
 var usersByDay = [817, 1370, 752, 1247, 681, 1120, 915, 1281, 875, 1341, 757, 610, 812, 1170, 769, 1261, 845, 1289, 515, 1247, 845, 1311, 741, 1239, 812, 638, 877, 1242, 1159, 1372];
 
-// Рисуем график посещаемости
+// Draw chart attendance
 keks.plot(usersByDay, expectedUsers);
 
-// Суммируем посещаемость
+// Summarize attendance
 var totalUsers = 0;
 var minUsers = expectedUsers - 100;
 for (var i = 0; i <= usersByDay.length - 1; i++) {
   totalUsers += usersByDay[i];
   if (usersByDay[i] < minUsers) {
-    console.log (usersByDay[i])
+    console.log (usersByDay[i]) // need to remove
     }
 }
 
