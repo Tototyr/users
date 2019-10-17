ba90bf062a2fcc3be0c4a1d6869a0af43b35c156
Myskov Vyacheslav <slmyskov@gmail.com>
2019-10-17 14:20:26 +0300

    remove undefined KEKS wtf

--- users.js
+++ users.js
@@ -1,9 +1,5 @@
 const expectedUsers = 1000;
-
 const usersByDay = [817, 1370, 752, 1247, 681, 1120, 915, 1281, 875, 1341, 757, 610, 812, 1170, 769, 1261, 845, 1289, 515, 1247, 845, 1311, 741, 1239, 812, 638, 877, 1242, 1159, 1372];
-
-// Draw chart attendance
-keks.plot(usersByDay, expectedUsers);
 
 // Summarize attendance
 let totalUsers = 0;
